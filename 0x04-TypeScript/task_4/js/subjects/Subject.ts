337f097d90f7cf22c0fe1d1b30f5fd50aec07e6a
SuNgu <cliffland.jasungu@gmail.com>
2025-08-18 13:34:55 +0300

    check files

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,15 +1,18 @@
-/// <reference path="./Teacher.ts" />
+// task_4/js/subjects/Subject.ts
 
+// Importing the Teacher interface from the Subjects namespace
+import { Teacher } from './Teacher';
+
+// Defining the Subject class in the Subjects namespace
 namespace Subjects {
     export class Subject {
-        teacher: Teacher;
+        private teacher: Teacher;
 
-        constructor(teacher: Teacher) {
-        this.teacher = teacher;
-        }
-
-        setTeacher(teacher: Teacher) {
+        // Setter method to assign a teacher to the subject
+        setTeacher(teacher: Teacher): void {
         this.teacher = teacher;
         }
     }
 }
+
+export default Subjects; // Exporting the Subjects namespace for use in other files
