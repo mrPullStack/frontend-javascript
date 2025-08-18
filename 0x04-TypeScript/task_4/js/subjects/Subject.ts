4ea5a3ae048ab9c6212e15d9e616c7394e6e9fea
SuNgu <cliffland.jasungu@gmail.com>
2025-08-18 13:15:55 +0300

    corrected final

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,7 +1,15 @@
 /// <reference path="./Teacher.ts" />
 
 namespace Subjects {
-    export interface Subject {
+    export class Subject {
         teacher: Teacher;
+
+        constructor(teacher: Teacher) {
+        this.teacher = teacher;
+        }
+
+        setTeacher(teacher: Teacher) {
+        this.teacher = teacher;
+        }
     }
 }
